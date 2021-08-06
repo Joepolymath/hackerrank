21ab9ba65245be53be3b2e6cfa3db2ba22dc8504
Joepolymath <joshuaajagbe@gmail.com>
2021-08-06 10:17:03 +0100

    first commit

--- app.py
+++ app.py
@@ -6,6 +6,19 @@ def check(a,b,c):
     else:
         return False
 
+# write a function that takes a number as input and returns true if the number is a prime number and false if it is not a prime number
+
+def isPrime(x):
+    num = 13
+    if num > 1:
+        for i in range(2, num//2):
+            if (num % i) == 0:
+                return False
+            else:
+                return True
+    else:
+        return False
 
 if __name__ == '__main__':
     print(check(2,3,3))
+    print(isPrime(3))
